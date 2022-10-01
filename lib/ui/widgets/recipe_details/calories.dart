bc2d2a84c1ecf734de520e48455c64c18c28a226
Deam <deam@aleph.engineering>
2022-10-01 13:30:59 -0400

    Bring calories.dart to stateful

--- lib/ui/widgets/recipe_details/calories.dart
+++ lib/ui/widgets/recipe_details/calories.dart
@@ -2,46 +2,59 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:recipe_app/data/models/models.dart';
 
-class Calories extends StatelessWidget {
+class Calories extends StatefulWidget {
   const Calories({
     Key? key,
-    required this.animation,
     required this.recipe,
   }) : super(key: key);
 
-  final Animation<double> animation;
   final Recipe recipe;
+
+  @override
+  State<Calories> createState() => _CaloriesState();
+}
+
+class _CaloriesState extends State<Calories>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     if (kDebugMode) {
       print('calories...');
     }
-    return AnimatedBuilder(
-      animation: animation,
-      builder: (context, child) => FadeTransition(
-        opacity: CurvedAnimation(
-            parent: animation, curve: const Interval(0.25, 1.0)),
-        child: child,
-      ),
-      child: Row(
-        children: [
-          const Icon(
-            Icons.local_fire_department,
-            size: 24,
-          ),
-          const SizedBox(
-            width: 3,
-          ),
-          FittedBox(
-            child: Text(
-              '${recipe.pricePerServing!.floor()} calories',
-              style: const TextStyle(
-                fontSize: 19,
+    return TweenAnimationBuilder(
+      tween: Tween(begin: 0.0, end: 1.0),
+      duration: const Duration(milliseconds: 500),
+      builder: (context, value, child) {
+        return Opacity(
+          opacity: value,
+          child: child,
+        );
+      },
+      child: FittedBox(
+        child: Row(
+          children: [
+            const Icon(
+              Icons.local_fire_department,
+              size: 24,
+            ),
+            const SizedBox(
+              width: 3,
+            ),
+            FittedBox(
+              child: Text(
+                '${widget.recipe.pricePerServing!.floor()} calories',
+                style: const TextStyle(
+                  fontSize: 19,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
