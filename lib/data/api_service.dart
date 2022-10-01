a80aad7c514774ffd2e8ef8084c7e3c6266145be
Deam <deam@aleph.engineering>
2022-10-01 13:24:50 -0400

    Accept only one recipe type for search

--- lib/data/api_service.dart
+++ lib/data/api_service.dart
@@ -4,7 +4,7 @@ abstract class APIService {
   APIService(this.baseUrl);
   final String baseUrl;
   Future<List<Recipe>?> getRecipes({Map<String, String>? queryParams});
-  Future<List<Recipe>?> searchRecipes({String? text, List<String>? tags}) {
+  Future<List<Recipe>?> searchRecipes({String? text, String? type}) {
     return Future.value(<Recipe>[]);
   }
 }
