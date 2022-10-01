e12f44cfcf0a2fd564b6bac119e05f1ae27c05df
Deam <deam@aleph.engineering>
2022-10-01 13:27:49 -0400

    Rename BaseUrls to RecipeUrls class

--- lib/data/network/endpoints.dart
+++ lib/data/network/endpoints.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class BaseUrls {
+class RecipeUrls {
   static String baseUrl = 'api.spoonacular.com';
   static get complexSearch => '/recipes/complexSearch';
   static get random => '/recipes/random';
