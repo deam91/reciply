383443db969bd6e81bf197d447450296d929b6e0
Deam <deam@aleph.engineering>
2022-10-01 13:23:51 -0400

    Add loading widget

---
+++ lib/core/widgets/loading_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder(
+      duration: const Duration(milliseconds: 250),
+      tween: Tween(begin: 0.0, end: 1.0),
+      builder: (context, value, child) {
+        return Opacity(
+          opacity: value,
+          child: child,
+        );
+      },
+      child: Center(
+        child: Image.asset(
+          'assets/searching.gif',
+          width: 200,
+          height: 200,
+        ),
+      ),
+    );
+  }
+}
