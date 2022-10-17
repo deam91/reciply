c868cd1cf6b5a689168f91c85a0864dfaec9d5c8
Deam <deam@aleph.engineering>
2022-10-17 12:20:45 -0400

    Move and include initial user info

---
+++ lib/profile/views/pages/profile_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/profile/views/widgets/user_brand.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text('Profile'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            iconSize: 24,
+            highlightColor: Colors.white12,
+            icon: const Icon(
+              Icons.more_horiz_rounded,
+              color: Colors.black87,
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: const [
+          UserBrand(),
+        ],
+      ),
+    );
+  }
+}
