06b96a48b3988fc74cb8eb1aba4dbf18879f02be
Deam <deam@aleph.engineering>
2022-11-04 18:59:35 -0400

    Refactor user profile service and provider

--- lib/profile/controllers/user_profile_provider.dart
+++ lib/profile/controllers/user_profile_provider.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:recipe_app/auth/controllers/auth_provider.dart';
+import 'package:recipe_app/dashboard/controllers/recipes_providers.dart';
 import 'package:recipe_app/profile/models/user_profile_service.dart';
 
 import '../../auth/models/user_profile.dart';
@@ -19,12 +21,22 @@ class UserProfileNotifier {
   UserProfile? get userProfile => _userProfile;
 
   Future<UserProfile?> getUserProfile() async {
+    if (kDebugMode) {
+      print('Getting user profile...');
+    }
     final user = ref.read(authControllerProvider).fbUser;
-    _userProfile = await userProfileService.getUserProfile(user?.uid);
-    return _userProfile;
+    final userProfile = await userProfileService.getUserProfile(user?.uid);
+    _userProfile = userProfile;
+    return userProfile;
   }
 }
 
 final userProfile = FutureProvider((ref) {
   return ref.read(userProfileProvider).getUserProfile();
+});
+
+final userRecipes = FutureProvider((ref) {
+  final user =
+      ref.watch(authControllerProvider.select((value) => value.fbUser));
+  return ref.read(recipeServiceProvider).userRecipes(userId: user?.uid);
 });
--- lib/profile/models/user_profile_service.dart
+++ lib/profile/models/user_profile_service.dart
@@ -1,15 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:recipe_app/auth/models/user_profile.dart';
 
 class UserProfileService {
   UserProfileService();
 
   Future<UserProfile?> getUserProfile(String? userUId) async {
-    final users = FirebaseFirestore.instance.collection('users').doc(userUId);
-    final snapshot = await users.get();
-    if (snapshot.exists) {
-      final data = snapshot.data() ?? {};
+    try {
+      final users = FirebaseFirestore.instance.collection('users').doc(userUId);
+      final user = await users.get();
+      final data = user.data() ?? {};
       return UserProfile.fromJson(data);
+    } on Exception catch (err) {
+      debugPrint(err.toString());
     }
     return null;
   }
