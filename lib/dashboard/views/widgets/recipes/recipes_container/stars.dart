3eccb8bfa205a3085b292d856b30bd8bce9cf0d8
Deam <deam@aleph.engineering>
2022-11-04 19:02:26 -0400

    Create recipe stars animated widget

---
+++ lib/dashboard/views/widgets/recipes/recipes_container/stars.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/dashboard/models/data/recipe.dart';
+import 'package:recipe_app/dashboard/views/widgets/recipe_details/stars.dart';
+
+class StarsWidget extends StatefulWidget {
+  const StarsWidget({super.key, required this.recipe});
+  final Recipe recipe;
+
+  @override
+  State<StarsWidget> createState() => _StarsWidgetState();
+}
+
+class _StarsWidgetState extends State<StarsWidget>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(milliseconds: 250),
+    vsync: this,
+  );
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeInOut,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _animation,
+      child: ScaleTransition(
+        scale: _animation,
+        child: Container(
+          padding: const EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: const Color(0xffFFE1B3).withOpacity(.7),
+          ),
+          height: 28,
+          child: Stars(
+            likes: widget.recipe.stars ?? 0,
+            size: 28,
+          ),
+        ),
+      ),
+    );
+  }
+}
