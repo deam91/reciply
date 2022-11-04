34890ce1155d57d009450c8b2dbc4c4486d7d181
Deam <deam@aleph.engineering>
2022-11-04 19:01:52 -0400

    Add z rotation for animated hero widget

--- lib/common/views/widgets/hero_widget.dart
+++ lib/common/views/widgets/hero_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HeroWidget extends StatelessWidget {
@@ -9,6 +11,37 @@ class HeroWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Hero(
       tag: tag,
+      flightShuttleBuilder: (
+        flightContext,
+        animation,
+        flightDirection,
+        fromHeroContext,
+        toHeroContext,
+      ) {
+        final tween = Tween(
+          begin: 0.001,
+          end: 2,
+        );
+        final curvedAnimation = CurvedAnimation(
+          parent: animation,
+          curve: Curves.easeOut,
+        );
+        final rotationAnimation = tween.animate(curvedAnimation);
+
+        return AnimatedBuilder(
+          animation: rotationAnimation,
+          child: child,
+          builder: (context, child) {
+            return Transform(
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.003)
+                ..rotateZ(rotationAnimation.value * pi),
+              alignment: Alignment.center,
+              child: child,
+            );
+          },
+        );
+      },
       child: Material(
         type: MaterialType.transparency,
         child: child,
