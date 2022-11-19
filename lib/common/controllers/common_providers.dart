1ec6aa34974c990e3366c899bc875a1833db330d
Deam <deam@aleph.engineering>
2022-11-19 05:26:36 -0500

    Add common providers

--- lib/common/controllers/common_providers.dart
+++ lib/common/controllers/common_providers.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:recipe_app/common/models/data/local_cache_service.dart';
-import 'package:recipe_app/common/models/data/notification_service.dart';
+import 'package:recipe_app/common/models/data_sources/local_cache_service.dart';
+import 'package:recipe_app/common/models/data_sources/notification_service.dart';
 
 final cacheProvider = StateProvider<LocalCacheService>((ref) {
   return LocalCacheService();
