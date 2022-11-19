992ecbfc66cfc8818d0c084bcade2536ab523cf3
Deam <deam@aleph.engineering>
2022-11-19 05:26:12 -0500

    Add recipe types and user settings models

---
+++ lib/common/models/data/recipe_types.dart
@@ -0,0 +1,14 @@
+// types: dessert, appetizer, salad, bread, breakfast, soup, beverage, sauce, marinade, snack, drink
+const List<String> recipeTypes = [
+  'dessert',
+  'appetizer',
+  'salad',
+  'bread',
+  'breakfast',
+  'soup',
+  'beverage',
+  'sauce',
+  'marinade',
+  'snack',
+  'drink'
+];
