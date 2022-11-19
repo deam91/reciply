d513f8ae79c13bb40c3853f4563ff4ea128dd506
Deam <deam@aleph.engineering>
2022-11-19 05:50:11 -0500

    feat(profile): add profile page

--- lib/profile/views/pages/profile_page.dart
+++ lib/profile/views/pages/profile_page.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:recipe_app/auth/controllers/auth_provider.dart';
 import 'package:recipe_app/profile/views/widgets/user_brand.dart';
 
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key}) : super(key: key);
+class ProfilePage extends ConsumerWidget {
+  const ProfilePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: const Text('Profile'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              ref.read(authControllerProvider.notifier).signOut();
+            },
             iconSize: 24,
             highlightColor: Colors.white12,
             icon: const Icon(
-              Icons.more_horiz_rounded,
+              Icons.logout_rounded,
               color: Colors.black87,
             ),
           )
