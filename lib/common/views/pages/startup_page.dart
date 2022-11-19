2a159425d5869dbff8020da76d30104d5578c98f
Deam <deam@aleph.engineering>
2022-11-19 05:31:55 -0500

    Refactor startup page

--- lib/common/views/pages/startup_page.dart
+++ lib/common/views/pages/startup_page.dart
@@ -1,27 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:recipe_app/auth/controllers/auth_provider.dart';
-import 'package:recipe_app/auth/views/login_page.dart';
+import 'package:recipe_app/auth/views/pages/login_page.dart';
 import 'package:recipe_app/common/controllers/common_providers.dart';
 import 'package:recipe_app/common/views/pages/home_page.dart';
 import 'package:recipe_app/dashboard/views/pages/onboarding_page.dart';
 
-class StartupPage extends ConsumerWidget {
+class StartupPage extends ConsumerStatefulWidget {
   const StartupPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final authStateChanges =
-        ref.watch(authControllerProvider.select((value) => value.status));
-    if (authStateChanges == Status.authenticated) {
-      return const HomePage();
-    } else {
-      final isOnboarded = ref.read(cacheProvider).isOnboarded();
-      if (!isOnboarded) {
-        return const OnboardingPage();
-      } else {
-        return const LoginPage();
-      }
+  ConsumerState<StartupPage> createState() => _StartupPageState();
+}
+
+class _StartupPageState extends ConsumerState<StartupPage> {
+  late final Future _isOnboarded;
+
+  @override
+  void initState() {
+    super.initState();
+    _isOnboarded = ref.read(cacheProvider).isOnboarded();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final authStateChanges = ref.watch(authControllerProvider);
+    print("BUILD START_PAGE");
+    switch (authStateChanges) {
+      case Status.authenticated:
+        return const HomePage();
+      case Status.loading:
+      case Status.uninitialized:
+      case Status.authenticating:
+      case Status.authenticatingWithGoogle:
+      case Status.unauthenticated:
+      case Status.authError:
+        return FutureBuilder(
+          future: _isOnboarded,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              final isOnboarded = snapshot.requireData;
+              if (!isOnboarded) {
+                return const OnboardingPage();
+              } else {
+                return const LoginPage();
+              }
+            }
+            return const LoginPage();
+          },
+        );
     }
   }
 }
