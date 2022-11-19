49402bb122026e6c2c535fc398c2d8b03c48e7a0
Deam <deam@aleph.engineering>
2022-11-19 05:54:27 -0500

    add(auth): sign up page

---
+++ lib/auth/views/pages/sign_up_page.dart
@@ -0,0 +1,55 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:recipe_app/auth/views/widgets/providers_login.dart';
+import 'package:recipe_app/auth/views/widgets/sign_up_form.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    print("BUILD LOGIN_PAGE");
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Create an account',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const Text(
+                  'Let’s help you set up your account, it won’t take long.',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                const SignUpForm(),
+                ProviderLogin(
+                  bottomText: 'Already a member?',
+                  bottomButtonText: 'Sign In',
+                  bottomButtonAction: () {
+                    context.router.pop();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
