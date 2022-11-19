1166eef12445384829609a0d68af27153248a66c
Deam <deam@aleph.engineering>
2022-11-19 18:22:44 -0500

    ref(recipes): initial image upload logic

--- lib/recipes/views/widgets/media_content.dart
+++ lib/recipes/views/widgets/media_content.dart
@@ -1,24 +1,29 @@
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:recipe_app/auth/controllers/auth_provider.dart';
 import 'package:recipe_app/common/models/constants.dart';
 
-class MediaContent extends StatefulWidget {
-  const MediaContent({super.key});
+class MediaContent extends ConsumerStatefulWidget {
+  const MediaContent({required this.onImageUploaded, super.key});
+  final Function(String) onImageUploaded;
 
   @override
-  State<MediaContent> createState() => _MediaContentState();
+  ConsumerState<MediaContent> createState() => _MediaContentState();
 }
 
-class _MediaContentState extends State<MediaContent> {
+class _MediaContentState extends ConsumerState<MediaContent> {
   PlatformFile? _platformFile;
   UploadTask? uploadTask;
 
   _uploadFile() async {
+    final userId = ref.read(authControllerProvider.notifier).fbUser?.uid;
     if (_platformFile != null) {
-      final path = '$firebaseImagesPath${_platformFile!.name}';
+      final path = '$firebaseImagesPath/$userId/${_platformFile!.name}';
       final file = File(_platformFile!.path!);
 
       final ref = FirebaseStorage.instance.ref().child(path);
@@ -28,6 +33,7 @@ class _MediaContentState extends State<MediaContent> {
       final snapshot = await uploadTask!.whenComplete(() {});
 
       final url = await snapshot.ref.getDownloadURL();
+      widget.onImageUploaded(url);
       print('Download Link: $url');
       setState(() {
         uploadTask = null;
@@ -55,8 +61,11 @@ class _MediaContentState extends State<MediaContent> {
           if (snapshot.hasData) {
             final data = snapshot.requireData;
             double progress = data.bytesTransferred / data.totalBytes;
-            return CircularProgressIndicator.adaptive(
-              value: 100 * progress,
+            print(progress * 100);
+            return Center(
+              child: CircularProgressIndicator(
+                value: progress,
+              ),
             );
           }
           return Container();
@@ -68,14 +77,15 @@ class _MediaContentState extends State<MediaContent> {
     return Stack(
       fit: StackFit.expand,
       children: [
+        // Image container
         Container(
           decoration: BoxDecoration(
             border: Border.all(
-              color: Colors.black,
+              color: Colors.white,
               width: .5,
             ),
             borderRadius: BorderRadius.circular(20),
-            color: Colors.black.withOpacity(.15),
+            color: Colors.white.withOpacity(.05),
           ),
           child: AnimatedSwitcher(
             duration: const Duration(milliseconds: 300),
@@ -91,19 +101,38 @@ class _MediaContentState extends State<MediaContent> {
             ),
           ),
         ),
+        // Close button
         Positioned(
           top: 10,
           right: 10,
           child: AnimatedSwitcher(
             duration: const Duration(milliseconds: 300),
             child: _platformFile != null
-                ? const Icon(
-                    Icons.close_rounded,
-                    color: Colors.white,
+                ? SizedBox(
+                    height: 30,
+                    width: 30,
+                    child: IconButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor:
+                              const Color(0xff129575).withOpacity(.5)),
+                      padding: const EdgeInsets.all(0),
+                      iconSize: 20,
+                      onPressed: () {
+                        setState(() {
+                          _platformFile = null;
+                          widget.onImageUploaded('');
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.close_rounded,
+                        color: Colors.white,
+                      ),
+                    ),
                   )
                 : null,
           ),
         ),
+        // Upload progress stream showing a circular progress indicator
         Positioned(
           left: 0,
           right: 0,
@@ -112,6 +141,7 @@ class _MediaContentState extends State<MediaContent> {
           child:
               uploadTask != null ? _buildProgress() : const SizedBox.shrink(),
         ),
+        // Text and button for open the file picker
         AnimatedSwitcher(
           duration: const Duration(milliseconds: 350),
           child: _platformFile == null
@@ -119,7 +149,7 @@ class _MediaContentState extends State<MediaContent> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     const Text(
-                      'Upload your media content',
+                      'Upload your recipe image',
                       style: TextStyle(color: Colors.white),
                     ),
                     const SizedBox(
@@ -131,20 +161,20 @@ class _MediaContentState extends State<MediaContent> {
                         TextButton.icon(
                           style: TextButton.styleFrom(
                             padding: const EdgeInsets.symmetric(horizontal: 15),
-                            backgroundColor: Colors.black.withOpacity(.4),
-                            foregroundColor: Colors.white.withOpacity(.8),
+                            backgroundColor: Colors.white.withOpacity(.85),
                           ),
                           onPressed: () {
                             _pickFile(FileType.image);
                           },
                           icon: const Icon(
                             Icons.image_search_rounded,
-                            color: Colors.white,
+                            color: Color(0xff129575),
                           ),
                           label: const Text(
                             'Image',
                             style: TextStyle(
-                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xff129575),
                             ),
                           ),
                         ),
