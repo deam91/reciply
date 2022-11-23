f8a72e587356a501b86ee0f76162d88263966424
Deam <deam@aleph.engineering>
2022-11-23 00:25:43 -0500

    feat(recipe_save): refactor recipe add form page  - add multiselect tags widget  - show loading indicator while saving the recipe  - implement save recipe ui logic

--- lib/recipes/views/widgets/add_form.dart
+++ lib/recipes/views/widgets/add_form.dart
@@ -1,6 +1,12 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:recipe_app/common/models/utils.dart';
+import 'package:recipe_app/dashboard/models/constants.dart';
 import 'package:recipe_app/dashboard/models/data/recipe.dart';
+import 'package:recipe_app/recipes/controllers/recipes_providers.dart';
 import 'package:recipe_app/recipes/views/widgets/form_field.dart';
+import 'package:recipe_app/recipes/views/widgets/multi_select_tags.dart';
 
 class AddRecipeForm extends StatefulWidget {
   const AddRecipeForm({super.key, required this.onFormSaved});
@@ -11,6 +17,9 @@ class AddRecipeForm extends StatefulWidget {
 }
 
 class _AddRecipeFormState extends State<AddRecipeForm> {
+  List<String> tags = [];
+  List<String> selectedTags = [];
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _summaryController = TextEditingController();
   final TextEditingController _servingsController = TextEditingController();
@@ -21,88 +30,181 @@ class _AddRecipeFormState extends State<AddRecipeForm> {
   @override
   void initState() {
     super.initState();
+    tags = TagEnum.values
+        .where((element) => element.value.toLowerCase() != 'all')
+        .map((e) => e.value)
+        .toList();
   }
 
+  // Validations for all fields
+  String? _textValidator(String? value) {
+    final text = Validator.isNotEmpty(value);
+    if (text != null) {
+      return text;
+    }
+    return null;
+  }
+
+  _saveButton() => ElevatedButton.icon(
+        onPressed: saveRecipe,
+        icon: const Icon(Icons.save_alt_rounded),
+        label: const Text('Save'),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white,
+          foregroundColor: const Color(0xff129575),
+          textStyle: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+          shape: const StadiumBorder(),
+        ),
+      );
+
+  _loadingButton() => ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white,
+          foregroundColor: const Color(0xff129575),
+          textStyle: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+          shape: const StadiumBorder(),
+        ),
+        child: const Center(
+          child: CircularProgressIndicator(
+            strokeWidth: 2,
+          ),
+        ),
+      );
+
   saveRecipe() {
-    // TODO: Create validations for all fields
-    // TODO: Validate form
-    // TODO: Create recipe object and call onFormSaved with it
+    debugPrint('saveRecipe()');
+    // Validate form
+    if (!formKey.currentState!.validate()) {
+      return;
+    }
+    formKey.currentState!.save();
+    // Create recipe object and call onFormSaved with it
+    final recipe = Recipe()
+      ..title = _titleController.text
+      ..summary = _summaryController.text
+      ..servings = int.parse(_servingsController.text)
+      ..calories = double.parse(_caloriesController.text)
+      ..readyInMinutes = int.parse(_readyInMinutesController.text)
+      ..tags = selectedTags;
+    widget.onFormSaved(recipe);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(
-          height: 8,
-        ),
-        RecipeFormField(
-          inputKey: const Key('new_recipe_title_form_field'),
-          controller: _titleController,
-          label: 'Title',
-          placeholder: 'Title...',
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        RecipeFormField(
-          inputKey: const Key('new_recipe_summary_form_field'),
-          controller: _summaryController,
-          label: 'Summary',
-          placeholder: 'Recipe summary...',
-          keyboardType: TextInputType.multiline,
-          maxLines: 4,
-          maxLength: 250,
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        RecipeFormField(
-          inputKey: const Key('new_recipe_servings_form_field'),
-          controller: _servingsController,
-          label: 'Servings',
-          placeholder: 'Amount of servings...',
-          keyboardType: TextInputType.number,
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        RecipeFormField(
-          inputKey: const Key('new_recipe_calories_form_field'),
-          controller: _caloriesController,
-          label: 'Calories',
-          placeholder: 'Amount of calories...',
-          keyboardType: TextInputType.number,
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        RecipeFormField(
-          inputKey: const Key('new_recipe_readyIn_form_field'),
-          controller: _readyInMinutesController,
-          label: 'Cook time (minutes)',
-          placeholder: 'Time in minutes...',
-          keyboardType: TextInputType.number,
-          keyboardAction: TextInputAction.done,
-        ),
-        const SizedBox(height: 20),
-        ElevatedButton.icon(
-          onPressed: () {},
-          icon: const Icon(Icons.save_alt_rounded),
-          label: const Text('Save'),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.white,
-            foregroundColor: const Color(0xff129575),
-            textStyle: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w600,
-            ),
-            shape: const StadiumBorder(),
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 8,
           ),
-        ),
-        const SizedBox(height: 20),
-      ],
+          MultiSelectTags(
+            tagList: tags,
+            onSelectionChanged: (tags) {
+              setState(() {
+                selectedTags = tags;
+              });
+            },
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          RecipeFormField(
+            inputKey: const Key('new_recipe_title_form_field'),
+            controller: _titleController,
+            label: 'Title',
+            placeholder: 'Title...',
+            validator: _textValidator,
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          RecipeFormField(
+            inputKey: const Key('new_recipe_summary_form_field'),
+            controller: _summaryController,
+            label: 'Summary',
+            placeholder: 'Recipe summary...',
+            keyboardType: TextInputType.multiline,
+            maxLines: 4,
+            maxLength: 250,
+            validator: _textValidator,
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          RecipeFormField(
+            inputKey: const Key('new_recipe_servings_form_field'),
+            controller: _servingsController,
+            label: 'Servings',
+            placeholder: 'Amount of servings...',
+            keyboardType: TextInputType.number,
+            validator: _textValidator,
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          RecipeFormField(
+            inputKey: const Key('new_recipe_calories_form_field'),
+            controller: _caloriesController,
+            label: 'Calories',
+            placeholder: 'Amount of calories...',
+            keyboardType: TextInputType.number,
+            validator: _textValidator,
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          RecipeFormField(
+            inputKey: const Key('new_recipe_readyIn_form_field'),
+            controller: _readyInMinutesController,
+            label: 'Cook time (minutes)',
+            placeholder: 'Time in minutes...',
+            keyboardType: TextInputType.number,
+            keyboardAction: TextInputAction.done,
+            validator: _textValidator,
+          ),
+          const SizedBox(height: 20),
+          // Watch changes and show loading indicator while saving.
+          Consumer(
+            builder: (context, ref, child) {
+              final status = ref.watch(recipeManagementProvider);
+              return status.when(
+                data: (data) {
+                  if (data == RecipeStatus.initial) {
+                    return _saveButton();
+                  }
+                  if (data == RecipeStatus.done) {
+                    Future.delayed(const Duration(milliseconds: 300), () {
+                      if (mounted) {
+                        context.router.pop('saved');
+                      }
+                    });
+                  }
+                  return const SizedBox.shrink();
+                },
+                error: (error, stack) {
+                  debugPrint(error.toString());
+                  showSnackBarMessage(context, error.toString());
+                  return _saveButton();
+                },
+                loading: () {
+                  debugPrint('Saving recipe...');
+                  return _loadingButton();
+                },
+              );
+            },
+          ),
+          const SizedBox(height: 20),
+        ],
+      ),
     );
   }
 }
