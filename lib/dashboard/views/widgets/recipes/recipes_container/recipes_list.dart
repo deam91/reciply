73f3c137fc5a1120c390294d2b9ebb7fcf7843fc
Deam <deam@aleph.engineering>
2022-11-23 00:08:30 -0500

    ref(ui): add transform animation to recipes page view

--- lib/dashboard/views/widgets/recipes/recipes_container/recipes_list.dart
+++ lib/dashboard/views/widgets/recipes/recipes_container/recipes_list.dart
@@ -1,10 +1,17 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:recipe_app/dashboard/models/data/recipe.dart';
 import 'package:recipe_app/dashboard/views/widgets/recipes/recipes_container/recipe.dart';
 
 class RecipesList extends StatefulWidget {
-  const RecipesList({super.key, required this.recipes});
+  const RecipesList({
+    super.key,
+    required this.recipes,
+    this.fromDashboard = false,
+  });
   final List<Recipe> recipes;
+  final bool fromDashboard;
 
   @override
   State<RecipesList> createState() => _RecipesListState();
@@ -13,11 +20,17 @@ class RecipesList extends StatefulWidget {
 class _RecipesListState extends State<RecipesList> {
   late final PageController pageController;
   int currentPage = 0;
+  double? currentPageValue = 0.0;
 
   @override
   void initState() {
     super.initState();
     pageController = PageController(initialPage: 0, viewportFraction: 0.65);
+    pageController.addListener(() {
+      setState(() {
+        currentPageValue = pageController.page;
+      });
+    });
   }
 
   @override
@@ -32,19 +45,25 @@ class _RecipesListState extends State<RecipesList> {
       onPageChanged: (page) => setState(() => currentPage = page),
       itemCount: widget.recipes.length,
       physics: const BouncingScrollPhysics(),
-      padEnds: false,
       controller: pageController,
       scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
-        return LayoutBuilder(
-          builder: (_, constraints) {
-            return RecipeWidget(
-              recipe: widget.recipes[index],
-              color: Colors.white,
-              size: constraints.smallest,
-              isCurrentPage: currentPage == index,
-            );
-          },
+        final scale = max(0.8, (1 - (currentPageValue! - index).abs()));
+
+        return Transform(
+          transform: Matrix4.identity()..scale(scale),
+          alignment: Alignment.center,
+          child: LayoutBuilder(
+            builder: (_, constraints) {
+              return RecipeWidget(
+                recipe: widget.recipes[index],
+                color: Colors.white,
+                size: constraints.smallest,
+                disableTouch: currentPage != index,
+                fromDashboard: widget.fromDashboard,
+              );
+            },
+          ),
         );
       },
     );
