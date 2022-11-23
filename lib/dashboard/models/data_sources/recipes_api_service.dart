2521add39c8e2a39094a9ed6640d2585ce9df15d
Deam <deam@aleph.engineering>
2022-11-23 00:19:55 -0500

    feat(save_recipe): add save function to recipes service

--- lib/dashboard/models/data_sources/recipes_api_service.dart
+++ lib/dashboard/models/data_sources/recipes_api_service.dart
@@ -1,23 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:recipe_app/auth/models/user_profile.dart';
 import 'package:recipe_app/common/models/data_sources/api_service.dart';
 import 'package:recipe_app/dashboard/models/constants.dart';
 import 'package:recipe_app/dashboard/models/data/recipe.dart';
 import 'package:recipe_app/search/controllers/search.dart';
 
 class RecipeService extends APIService {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  RecipeService();
+  RecipeService({required this.firestore});
+  final FirebaseFirestore firestore;
 
   @override
   Future<List<Recipe>?> getRecipes({TagEnum? tag}) async {
     try {
-      var query = _firestore.collection('recipes').limit(10);
-      if (tag != TagEnum.all) {
+      var query = firestore.collection('recipes').limit(10);
+      if (tag != null && tag != TagEnum.all) {
         query = query
-            .where('tags', arrayContains: tag?.value)
-            .orderBy("createdAt", descending: true)
-            .limit(10);
+            .where('tags', arrayContains: tag.value)
+            .orderBy("createdAt", descending: true);
       }
       final snapshot = await query.get();
       final list = snapshot.docs.map((e) {
@@ -34,7 +34,7 @@ class RecipeService extends APIService {
   Future<List<Recipe>?> searchRecipes(
       {SearchFilter? filters, String text = ''}) async {
     try {
-      var query = _firestore.collection('recipes').limit(10);
+      var query = firestore.collection('recipes').limit(10);
 
       if (text != '') {
         query = query
@@ -87,7 +87,7 @@ class RecipeService extends APIService {
 
   Future<List<Recipe>?> userRecipes({String? userId = ''}) async {
     try {
-      var query = _firestore.collection('recipes').limit(10);
+      var query = firestore.collection('recipes').limit(10);
 
       if (userId != '') {
         query = query
@@ -104,5 +104,19 @@ class RecipeService extends APIService {
       debugPrint(e.toString());
     }
     return null;
+  }
+
+  Future<DocumentReference<Map<String, dynamic>>> saveRecipe({
+    required Recipe recipe,
+    required UserProfile? owner,
+    required String? userId,
+  }) async {
+    recipe.ownerId = userId;
+    recipe.owner = RecipeOwner()
+      ..name = owner?.name
+      ..image = owner?.photoUrl;
+    return await FirebaseFirestore.instance
+        .collection('recipes')
+        .add(recipe.toJson());
   }
 }
