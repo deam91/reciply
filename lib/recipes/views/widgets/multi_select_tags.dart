8015844b8860a40e0af628dd6d1c4e4a952b2ec6
Deam <deam@aleph.engineering>
2022-11-23 00:22:54 -0500

    feat(recipe_save): add multiselect tags to add recipe page

---
+++ lib/recipes/views/widgets/multi_select_tags.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class MultiSelectTags extends StatefulWidget {
+  const MultiSelectTags(
+      {super.key, required this.tagList, required this.onSelectionChanged});
+  final List<String> tagList;
+  final Function(List<String>) onSelectionChanged;
+
+  @override
+  MultiSelectTagsState createState() => MultiSelectTagsState();
+}
+
+class MultiSelectTagsState extends State<MultiSelectTags> {
+  bool isSelected = false;
+  List<String> selectedTags = <String>[];
+
+  _buildChoiceList() {
+    return widget.tagList.map((item) {
+      final selected = selectedTags.contains(item);
+      return Container(
+        padding: const EdgeInsets.all(2.0),
+        child: ChoiceChip(
+          selectedColor: const Color(0xff129575).withOpacity(.8),
+          backgroundColor: const Color(0xff129575),
+          labelPadding: const EdgeInsets.all(0),
+          padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
+          labelStyle: const TextStyle(color: Colors.white),
+          side: selected
+              ? null
+              : BorderSide(
+                  color: Colors.white.withOpacity(.1),
+                ),
+          label: Text(item),
+          shape: const StadiumBorder(),
+          selected: selected,
+          onSelected: (_) {
+            setState(() {
+              selectedTags.contains(item)
+                  ? selectedTags.remove(item)
+                  : selectedTags.add(item);
+            });
+            widget.onSelectionChanged(selectedTags);
+          },
+        ),
+      );
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Text(
+          'Recipe Tags',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Wrap(
+          children: _buildChoiceList(),
+        ),
+      ],
+    );
+  }
+}
