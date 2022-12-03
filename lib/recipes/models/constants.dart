acaef9f5710245864f05c5fd27ee6d17ac403895
Deam <deam@aleph.engineering>
2022-12-03 15:53:19 -0500

    Add constants

--- lib/recipes/models/constants.dart
+++ lib/recipes/models/constants.dart
@@ -1,3 +1,7 @@
 import 'package:recipe_app/dashboard/models/data/recipe.dart';
 
-typedef RecipeCallback = Function(Recipe recipe);
+typedef GoToPageCallback = void Function({int? index});
+typedef RecipeCallback = void Function(Recipe recipe, {int? index});
+typedef IngredientCallback = void Function(Ingredient ingredient);
+typedef InstructionCallback = void Function(Instruction ingredient);
+typedef TextCallback = void Function(String? value);
